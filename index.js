a6cc7cf02b3171101b48d825f59f9b1ac37dbfc5
Christian Friedow <christian@friedow.com>
2017-07-31 17:49:29 +0200

    Added profile command

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Discord = require('discordie');
 
 class Kirito {
-  
+
   constructor() {
     this.Events = Discord.Events;
     this.ApiClient = new Discord();
     this.ApiClient.connect({
     	token: "MzQwNDE5MjU4MDQ1NTYyODgy.DFyPtw.T-i9DfVm2-bbcZ3Nc6E1kJ-DLqY"
     });
-    this.subscribeToEvents()
+    this.subscribeToEvents();
   }
 
   subscribeToEvents() {
@@ -30,6 +30,12 @@ class Kirito {
       console.log('Currently connected users:')
       this.getCurrentlyConnectedUsers().map( (user) => {
         console.log('- ' + user.name);
+        // DATABASE request user;
+        // IF user:
+          // user.level += 5000;
+        // ELSE:
+          // user.level = 5000;
+        // DATABASE write user;
       });
       console.log('')
     }, 5000);
@@ -51,6 +57,15 @@ class Kirito {
     if (e.message.content == 'ping') {
       e.message.channel.sendMessage('pong');
     }
+    else if (e.message.content == 'profile') {
+      const profile = this.getProfile(e.message.user);
+      e.message.channel.sendMessage(profile);
+    }
+  }
+
+  getProfile(user) {
+
+    return 'test';
   }
 
   countServers() {
