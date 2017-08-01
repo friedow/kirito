bbdebc8acc3fe0af3e66063753c4d336e763212e
Christian Friedow <christian@friedow.com>
2017-08-01 17:27:14 +0200

    Added handlebars support

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const Discord = require('discordie');
 const handlebars = require('handlebars');
-var webshot = require('webshot');
 const Screenshot = require('screenshot-stream');
 
 class Kirito {
@@ -57,14 +56,41 @@ class Kirito {
       e.message.channel.sendMessage('pong');
     }
     else if (e.message.content == 'profile') {
-      const profile = this.getProfile(e.message.user);
+      const profile = this.getProfile(e.message.author);
       e.message.channel.uploadFile(profile, 'profile.png');
     }
   }
 
   getProfile(user) {
     // TODO: LOAD Level Information from database
-    const stream = Screenshot('Profile/Profile.html', '500x1000', {crop: true, selector: '.profile'});
+    const source = fs.readFileSync('Profile/Profile.html').toString();
+    const context = {
+      username: user.username,
+      level: "13",
+      levelProgress: "1%",
+      avatar: user.staticAvatarURL,
+      achievements: [
+        {
+          icon: "11",
+          title: "Creator of Kirito",
+          description: "Highest rank ever reachable"
+        },
+        {
+          icon: "14",
+          title: "Level? Over 9000!",
+          description: "You asked for it XP"
+        },
+        {
+          icon: "27",
+          title: "Ridiculouosly Overpowered",
+          description: "Title sais everything"
+        }
+      ]
+    };
+    const template = handlebars.compile(source);
+    const html = template(context);
+    fs.writeFileSync('profiles/Notoxus.html', html);
+    const stream = Screenshot('profiles/Notoxus.html', '500x1000', {crop: true, selector: '.profile'});
     return stream;
   }
 
