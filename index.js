b2758b647d52d871febd651d2149a717d37359d5
Christian Friedow <christian@friedow.com>
2017-08-05 11:36:45 +0200

    Enhanced addExperience function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const Discord = require('discordie');
 const handlebars = require('handlebars');
 const Screenshot = require('screenshot-stream');
+const MongoClient = require('mongodb').MongoClient;
+const databaseUrl = "mongodb://localhost:27017/test";
 
 class Kirito {
 
@@ -11,21 +13,79 @@ class Kirito {
     this.ApiClient.connect({
     	token: "MzQwNDE5MjU4MDQ1NTYyODgy.DFyPtw.T-i9DfVm2-bbcZ3Nc6E1kJ-DLqY"
     });
+    this.connectedUsers = [];
+    this.setGame = this.setGame.bind(this);
+    this.handleUserConnectedToVoice = this.handleUserConnectedToVoice.bind(this);
+    this.handleUserDisconnectedFromVoice = this.handleUserDisconnectedFromVoice.bind(this);
+    this.handleChatCommand = this.handleChatCommand.bind(this);
     this.subscribeToEvents();
+
   }
 
   subscribeToEvents() {
-    this.ApiClient.Dispatcher.on(this.Events.GATEWAY_READY, (e) => {
-      this.setGame();
-      this.watchUsers();
-    });
-    this.ApiClient.Dispatcher.on(this.Events.MESSAGE_CREATE, (e) => {
-    	this.handleChatCommand(e);
-    });
+    this.ApiClient.Dispatcher.on(this.Events.GATEWAY_READY, this.setGame);
+    this.ApiClient.Dispatcher.on(this.Events.VOICE_CHANNEL_JOIN, this.handleUserConnectedToVoice);
+    this.ApiClient.Dispatcher.on(this.Events.VOICE_CHANNEL_LEAVE, this.handleUserDisconnectedFromVoice);
+    this.ApiClient.Dispatcher.on(this.Events.MESSAGE_CREATE, this.handleChatCommand);
   }
 
-  setGame() {
+  setGame(e) {
     this.ApiClient.User.setGame('together with Asuna');
+  }
+
+  handleUserConnectedToVoice(e) {
+    const user = JSON.parse(JSON.stringify(e.user));
+    user.timeConnected = Date.now();
+    this.connectedUsers[e.user.id] = user;
+    console.log("User " + e.user.username + " connected to voice channel " + e.channel.name + " on server " + e.channel.guild.name + ".");
+  }
+
+  handleUserDisconnectedFromVoice(e) {
+    const onlineTime = this.calculateVoiceTime(e.user);
+    delete this.connectedUsers[e.user.id];
+    this.addExperience(e.user, onlineTime);
+    console.log("User " + e.user.username + " disconnected from voice channel " + e.channel.name + " on server " + e.channel.guild.name + ".");
+  }
+
+  calculateVoiceTime(user) {
+    const timeDisconnected = Date.now();
+    const currentUser = this.connectedUsers[user.id];
+    if (currentUser) {
+      return Math.round((timeDisconnected - currentUser.timeConnected) / 60000);
+    }
+    else {
+      console.log("User " + user.username + " not found in list of online users. 0 minutes online.");
+      return 0;
+    }
+  }
+
+  addExperience(currentUser, experience) {
+    MongoClient.connect(databaseUrl, (err, db) => {
+      if (err) throw err;
+      db.collection("users").findOne({id: currentUser.id}, (err, result) => {
+        if (err) throw err;
+        let user = {};
+        if (!result) {
+          user = JSON.parse(JSON.stringify(currentUser));
+          user.experience = 0;
+          db.collection("users").insertOne(user, (err, res) => {
+            if (err) throw err;
+            console.log("Added user " + user.username + " to databse.");
+          });
+        }
+        else {
+          user = result;
+          console.log("Found user: " + user.username + " in database.");
+        }
+        user.experience += experience;
+        db.collection("users").updateOne({id: user.id}, user, function(err, res) {
+          if (err) throw err;
+          console.log("Added " + experience + " experience to user " + user.username + ".");
+          console.log(user.username + " has now a total experience of " + user.experience + ".");
+        });
+        db.close();
+      });
+    });
   }
 
   watchUsers() {
