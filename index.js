a24b982372625bc5f674b43046299489dcbb5245
Christian Friedow <christian@friedow.com>
2017-08-05 11:35:11 +0200

    Removed hard coded stuff

--- index.js
+++ index.js
@@ -56,28 +56,12 @@ class Kirito {
       e.message.channel.sendMessage('pong');
     }
     else if (e.message.content == 'profile') {
-      e.message.channel.uploadFile(profile, 'profile.png');
       const profile = this.getProfile(e.message.author, (profile) => {
+        e.message.channel.uploadFile(profile, 'profile.png');
       });
     }
   }
 
-    const source = fs.readFileSync('Profile/Profile.html').toString();
-    const context = {
-      username: user.username,
-      achievements: [
-        {
-          icon: "11",
-          description: "Highest rank ever reachable"
-        },
-        {
-          icon: "14",
-          description: "You asked for it XP"
-        },
-        {
-          icon: "27",
-          title: "Ridiculouosly Overpowered",
-          description: "Title sais everything"
   getProfile(author, cb) {
     MongoClient.connect(databaseUrl, (err, db) => {
       if (err) throw err;
@@ -114,11 +98,36 @@ class Kirito {
         const experienceOfCurrentLevel = user.experience - totalExperienceForCurrentLevel;
         const currentLevelProgress = experienceOfCurrentLevel * 100 / experienceForCurrentLevel;
 
+        const context = {
+          username: user.username,
           level: currentLevel,
           levelProgress: currentLevelProgress,
           avatar: author.staticAvatarURL,
+          achievements: [
+            {
+              icon: "11",
               title: "The One Award",
+              description: "Highest rank ever reachable"
+            },
+            {
+              icon: "14",
               title: "Skill Level? Over 9000!",
+              description: "You asked for it XP"
+            },
+            {
+              icon: "27",
+              title: "Ridiculouosly Overpowered",
+              description: "Title sais everything"
+            }
+          ]
+        };
+        const template = handlebars.compile(source);
+        const html = template(context);
+        fs.writeFileSync('profiles/Notoxus.html', html);
+        const stream = Screenshot('profiles/Notoxus.html', '500x1000', {crop: true, selector: '.profile'});
+        cb(stream);
+      });
+    });
   }
 
   countServers() {
