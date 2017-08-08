f73a0a2fab0c08c64a58ce6856d3404b6fc77929
Christian Friedow <christian@friedow.com>
2017-08-08 16:34:19 +0200

    Refactored index js

--- index.js
+++ index.js
@@ -1,89 +1,128 @@
 const fs = require('fs');
-const Discord = require('discordie');
+const mongojs = require('mongojs');
+const discord = require('discordie');
 const handlebars = require('handlebars');
 const Screenshot = require('screenshot-stream');
-const mongojs = require('mongojs');
-
 
 class Kirito {
-
   constructor() {
-    this.ApiClient = new Discord();
-    this.ApiClient.connect({
-    	token: process.env.AUTH_TOKEN
-    });
-    this.db = mongojs('mongo/kirito', ['users']);
+    // Prepare discord api
+    this.discordApi = new discord();
+    this.discordApi.connect({ token: process.env.AUTH_TOKEN });
+
+    // Prepare database
+    this.db = mongojs('kirito', ['users']);
+
+    // Prepare application state
     this.connectedUsers = [];
-    this.setGame = this.setGame.bind(this);
-    this.handleUserConnectedToVoice = this.handleUserConnectedToVoice.bind(this);
-    this.handleUserDisconnectedFromVoice = this.handleUserDisconnectedFromVoice.bind(this);
-    this.handleChatCommand = this.handleChatCommand.bind(this);
     this.subscribeToEvents();
   }
 
   subscribeToEvents() {
-    this.ApiClient.Dispatcher.on(Discord.Events.GATEWAY_READY, this.setGame);
-    this.ApiClient.Dispatcher.on(Discord.Events.VOICE_CHANNEL_JOIN, this.handleUserConnectedToVoice);
-    this.ApiClient.Dispatcher.on(Discord.Events.VOICE_CHANNEL_LEAVE, this.handleUserDisconnectedFromVoice);
-    this.ApiClient.Dispatcher.on(Discord.Events.MESSAGE_CREATE, this.handleChatCommand);
+    // Bind this into callback functions
+    this.setGame = this.setGame.bind(this);
+    this.addConnectedUser = this.addConnectedUser.bind(this);
+    this.removeConnectedUser = this.removeConnectedUser.bind(this);
+    this.handleChatCommand = this.handleChatCommand.bind(this);
+
+    // Subscribe to events
+    this.discordApi.Dispatcher.on(discord.Events.GATEWAY_READY, this.setGame);
+    this.discordApi.Dispatcher.on(discord.Events.VOICE_CHANNEL_JOIN, this.addConnectedUser);
+    this.discordApi.Dispatcher.on(discord.Events.VOICE_CHANNEL_LEAVE, this.removeConnectedUser);
+    this.discordApi.Dispatcher.on(discord.Events.MESSAGE_CREATE, this.handleChatCommand);
   }
 
   setGame(e) {
-    this.ApiClient.User.setGame('together with Asuna');
+    this.discordApi.User.setGame('together with Asuna');
   }
 
-  handleUserConnectedToVoice(e) {
+  addConnectedUser(e) {
     const user = JSON.parse(JSON.stringify(e.user));
-    user.timeConnected = Date.now();
+    user.dateConnected = Date.now();
     this.connectedUsers[e.user.id] = user;
-    console.log("User " + e.user.username + " connected to voice channel " + e.channel.name + " on server " + e.channel.guild.name + ".");
+    console.log('User ' + e.user.username + ' connected to voice channel ' + e.channel.name + ' on server ' + e.channel.guild.name + '.');
   }
 
-  handleUserDisconnectedFromVoice(e) {
-    const onlineTime = this.calculateVoiceTime(e.user);
+  removeConnectedUser(e) {
+    const experience = this.calculateVoiceTime(e.user);
+    this.addExperience(e.user, e.channel.guild, experience);
     delete this.connectedUsers[e.user.id];
-    this.addExperience(e.user, e.channel.guild, onlineTime);
-    console.log("User " + e.user.username + " disconnected from voice channel " + e.channel.name + " on server " + e.channel.guild.name + ".");
+    console.log('User ' + e.user.username + ' disconnected from voice channel ' + e.channel.name + ' on server ' + e.channel.guild.name + '.');
   }
 
   calculateVoiceTime(user) {
-    const timeDisconnected = Date.now();
-    const currentUser = this.connectedUsers[user.id];
-    if (currentUser) {
-      return Math.round((timeDisconnected - currentUser.timeConnected) / 60000);
-    }
-    else {
-      console.log("User " + user.username + " not found in list of online users. 0 minutes online.");
+    const dateDisconnected = Date.now();
+    const connectedUser = this.connectedUsers[user.id];
+    if (connectedUser) {
+      return Math.round((dateDisconnected - connectedUser.dateConnected) / 60000);
+    } else {
+      console.log('User ' + user.username + ' not found in list of online users. 0 minutes online.');
       return 0;
     }
   }
 
   addExperience(user, server, experience) {
-    const databaseUser = JSON.parse(JSON.stringify(user));
-    this.db.users.update({id: databaseUser.id, "servers.id": server.id}, {$inc: {"servers.$.experience": experience}} );
-    this.db.users.update({id: databaseUser.id, "servers.id": {$ne: server.id}}, { $setOnInsert: databaseUser, $inc: { experience: experience }, $push: {"servers": {id: server.id, "experience": experience}} }, {upsert: true}, (err, res) => {
-      if (err) throw err;
-      console.log("Added " + experience + " experience to user " + user.username + ".");
-      console.log(user.username + " has now a total experience of " + res.experience + ".");
-
+    this.db.users.update({id: user.id, 'servers.id': server.id}, {$inc: {'servers.$.experience': experience}} );
+    this.db.users.update({id: user.id, 'servers.id': {$ne: server.id}}, { $setOnInsert: user, $inc: { experience: experience }, $push: {'servers': {id: server.id, 'experience': experience}} }, {upsert: true, new: true}, (err, res) => {
+      console.log('Added ' + experience + ' experience to user ' + user.username + '.');
+      console.log(user.username + ' has now a total experience of ' + res.experience + '.');
     });
   }
 
   handleChatCommand(e) {
-    if (e.message.content == 'ping') {
-      e.message.channel.sendMessage('pong');
+    switch (e.message.content) {
+      case 'ping':
+        e.message.channel.sendMessage('pong');
+        break;
+
+      case 'profile':
+        this.getProfile(e.message.author, (profile) => {
+          e.message.channel.uploadFile(profile, 'profile.png');
+        });
+        break;
     }
-    else if (e.message.content == 'profile') {
-      const profile = this.getProfile(e.message.author, (profile) => {
-        e.message.channel.uploadFile(profile, 'profile.png');
-      });
-    }
+  }
+
+  getProfile(user, callback) {
+    const newUser = JSON.parse(JSON.stringify(user));
+    newUser.experience = 0;
+    console.log('Printing profile for user ' + user.username + '.');
+    this.db.users.findAndModify({query: {id: user.id}, update: { $setOnInsert: newUser }, upsert: true, new: true}, (err, result) => {
+      const profileInformation = {
+        username: user.username,
+        level: this.calculateLevel(result.experience);,
+        levelProgress: this.calculateLevelProgress(result.experience),
+        avatar: user.staticAvatarURL,
+        servers: this.getAdditionalServerData(result.servers)
+      };
+      const profileHtmlFilename = 'profiles/' + user.username + '.html';
+      this.createProfileHtml(profileHtmlFilename, profileInformation);
+      const stream = this.createProfileImageStream(profileHtmlFilename);
+      callback(stream);
+    });
+  }
+
+  calculateLevel(experience) {
+    return Math.round(Math.sqrt(result.experience / 3 + 36) - 5);
+  }
+
+  calculateLevelProgress(experience) {
+    const userLevel = this.calculateLevel(experience);
+    const currentLevelMinExperience = this.minLevelExperience(userLevel);
+    const nextLevelMinExperience = this.minLevelExperience(userLevel + 1);
+    const currentLevelExperience = nextLevelMinExperience - currentLevelMinExperience;
+    const userLevelExperience = experience - currentLevelMinExperience;
+    return userLevelExperience * 100 / currentLevelExperience;
+  }
+
+  minLevelExperience(level) {
+    return (3 * level * (level + 10) - 33);
   }
 
   getAdditionalServerData(servers) {
     let serverList = [];
     servers.map((currentServer) => {
-      let server = this.ApiClient.Guilds.get(currentServer.id);
+      const server = this.discordApi.Guilds.get(currentServer.id);
       const databaseServer = JSON.parse(JSON.stringify(server));
       databaseServer.experience = currentServer.experience;
       serverList.push(databaseServer);
@@ -92,42 +131,15 @@ class Kirito {
     return serverList;
   }
 
-  getProfile(user, callback) {
-    console.log("Printing profile for user " + user.username + ".");
-    const databaseUser = JSON.parse(JSON.stringify(user));
-    databaseUser.experience = 0;
-    this.db.users.findAndModify({query: {id: user.id}, update: { $setOnInsert: databaseUser }, upsert: true, new: true}, (err, result) => {
-      const source = fs.readFileSync('Profile/Profile.html').toString();
-      // Calculate current level
-      const currentLevel = Math.round(Math.sqrt(result.experience / 3 + 36) - 5);
-      // Calculate level progress
-      const nextLevel = currentLevel + 1;
-      const totalExperienceForCurrentLevel = 3 * currentLevel * (currentLevel + 10) - 33;
-      const totalExperienceForNextLevel = 3 * nextLevel * (nextLevel + 10) - 33;
-      const experienceForCurrentLevel = totalExperienceForNextLevel - totalExperienceForCurrentLevel;
-      const experienceOfCurrentLevel = databaseUser.experience - totalExperienceForCurrentLevel;
-      const currentLevelProgress = experienceOfCurrentLevel * 100 / experienceForCurrentLevel;
-
-      const servers = this.getAdditionalServerData(result.servers);
-
-      const context = {
-        username: databaseUser.username,
-        level: currentLevel,
-        levelProgress: currentLevelProgress,
-        avatar: user.staticAvatarURL,
-        servers: servers
-      };
-      const template = handlebars.compile(source);
-      const html = template(context);
-      const filename = 'profiles/' + databaseUser.username + '.html';
-      fs.writeFileSync(filename, html);
-      const stream = Screenshot(filename, '500x1000', {crop: true, selector: '.profile'});
-      callback(stream);
-    });
+  createProfileHtml(filename, profileInformation) {
+    const templateFile = fs.readFileSync('Profile/Profile.html').toString();
+    const template = handlebars.compile(templateFile);
+    const html = template(profileInformation);
+    fs.writeFileSync(filename, html);
   }
 
-  countServers() {
-    return this.ApiClient.Guilds.length;
+  createProfileImageStream(filename) {
+    return Screenshot(filename, '500x1000', {crop: true, selector: '.profile'});
   }
 
 }
