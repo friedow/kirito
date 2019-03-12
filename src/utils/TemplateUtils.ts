28646fc19499e4510b9b015ef01efc7a401f8892
Christian Friedow <christian@friedow.com>
2019-03-12 12:44:23 +0100

    Fixed paths

--- src/utils/TemplateUtils.ts
+++ src/utils/TemplateUtils.ts
@@ -2,25 +2,24 @@ import fs from 'fs';
 import handlebars from 'handlebars';
 import Pageres from 'pageres';
 import path from 'path';
-import Profile from '@/interfaces/Profile';
 import Toplist from '@/interfaces/Toplist';
 import User from '@/utils/User';
 
 export default class TemplateUtil {
   public static async getProfilePng(user: User): Promise<string> {
-    const profileTemplateFile = `${process.cwd()}/src/interface/templates/profile.html`;
+    const profileTemplateFile = `${__dirname}/../interface/templates/profile.html`;
     return TemplateUtil.hbsToPng(profileTemplateFile, user);
   }
 
   public static async getToplistPng(toplist: Toplist): Promise<string> {
-    const toplistTemplateFile = `${process.cwd()}/src/interface/templates/toplist.html`;
+    const toplistTemplateFile = `${__dirname}/../interface/templates/toplist.html`;
     return TemplateUtil.hbsToPng(toplistTemplateFile, toplist);
   }
 
   private static async hbsToPng(templateFile: string, templateContext: any): Promise<string> {
     const randomFilename = Math.random().toString(36).substring(7);
-    const htmlFile = `${process.cwd()}/src/interface/${randomFilename}.html`;
-    const pngFile = `${process.cwd()}/src/interface/${randomFilename}.png`;
+    const htmlFile = `${__dirname}/../interface/${randomFilename}.html`;
+    const pngFile = `${__dirname}/../interface/${randomFilename}.png`;
 
     TemplateUtil.hbsToHtml(templateFile, templateContext, htmlFile);
     await TemplateUtil.htmlToPng(htmlFile, pngFile);
